614e8f4876858aaa43fc91ffb47a6a8858aa6991
Oliver Dixon <olly257@gmail.com>
2022-08-10 15:02:08 +0200

    update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
-  import "./app.css";
-  import { onMount } from "svelte";
+    import './app.css'
+    import { onMount } from 'svelte'
 
-  onMount(async () => {
-    var video = document.querySelector("#videoElement");
+    onMount(async () => {
+        const video = document.querySelector(
+            '#videoElement'
+        ) as HTMLVideoElement
 
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
-        .catch(function (err0r) {
-          console.log("Something went wrong!");
-        });
-    }
-  });
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices
+                .getUserMedia({ video: true })
+                .then(function (stream) {
+                    video.srcObject = stream
+                })
+                .catch(function (err0r) {
+                    console.log('Something went wrong!')
+                })
+        }
+    })
 </script>
 
 <main class="mt-16 ml-4">
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-  <video autoplay={true} id="videoElement" class="w-44 h-44 bg-slate-400" />
+    <video autoplay={true} id="videoElement" class="w-44 h-44 bg-slate-400" />
 </main>
 
 <style>
