b5058fd02bdbfebccc3ba39fb9557fa9d47fdab7
Oliver Dixon <olly257@gmail.com>
2022-08-18 23:45:48 +0200

    update capacitor.config.ts, AppDelegate.swift and package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,7 @@ const config: CapacitorConfig = {
     server: {
         url: 'http://localhost:5173/',
         // url: 'http://10.0.2.2:5173', // Android
+        // url: 'http://192.168.50.38:5173/',
         cleartext: true,
     },
 }
--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
@@ -27,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Override point for customization after application launch.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
